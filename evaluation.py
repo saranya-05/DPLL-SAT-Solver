ad65cd11589fb726c39d4a7e6c18e10c90be3599
Saranya Kavileswarapu <54995646+saranya-05@users.noreply.github.com>
2023-10-01 17:30:55 -0400

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -3,14 +3,6 @@ import random
 import time
 from sat_dpll import dpll_eval,dpll_calls
 
-# DPLL Solver (Implement your solvers here)
-
-def is_satisfiable(cnf):
-    # Implement your DPLL solver logic here
-    pass
-
-# Generate Random 3-SAT Instances
-
 def generate_random_3sat(N, L):
     clauses = []
     for _ in range(L):
@@ -24,53 +16,31 @@ def generate_random_3sat(N, L):
         clauses.append(clause)
     return clauses
 
-# Performance Evaluation
-
 def evaluate_solver(N, L, num_experiments,h):
-    
     satisfiable_count = 0
     total_time = 0
     for _ in range(num_experiments):
-        # Generate a random 3-SAT instance
         cnf = generate_random_3sat(N, L)
-        # Measure execution time
         start_time = time.time()
         satisfiable = dpll_eval(cnf,h)
         end_time = time.time()
-
         total_time += end_time - start_time
-
-        # Count satisfiable instances
         if satisfiable:
             satisfiable_count += 1
-
-    # Calculate probability of satisfiability
     satisfiability_probability = satisfiable_count / num_experiments
-
-    # Calculate median execution time
     median_execution_time = total_time / num_experiments
-
     return satisfiability_probability, median_execution_time
 
-# Main Evaluation Loop
-
 if __name__ == '__main__':
-    # Define parameters
-    N_values = [100,150, 200]  # Vary the number of variables
-    L_N_ratios = [3.0, 3.2, 3.4, 3.6,3.8,4.0,4.2,4.4,4.6,4.8,5.0,5.2,5.4,5.6,5.8,6.0]  # Vary the L/N ratio
-    num_experiments = 100  # Number of experiments per combination
-    print("Enter heuristic: 1.random-choice, 2.2-clause, 2.max-literal-weight")
+    N_values = [100,150,200]  
+    L_N_ratios = [3.0, 3.2, 3.4, 3.6,3.8,4.0,4.2,4.4,4.6,4.8,5.0,5.2,5.4,5.6,5.8,6.0]  
+    num_experiments = 100 
+    print("Enter heuristic: 1.random-choice, 2.2-clause, 3.max-literal-weight")
     h=int(input())
-
     for N in N_values:
         for L_N_ratio in L_N_ratios:
-            # Calculate the number of clauses (L) based on the L/N ratio
             L = int(N * L_N_ratio)
-
-            # Perform the evaluation
             satisfiability_probability, median_execution_time = evaluate_solver(N, L, num_experiments,h)
-
-            # Print results
             print(f"N={N}, L/N={L_N_ratio:.2f}, L={L}:")
             print(f"Probability of Satisfiability: {satisfiability_probability:.2f}")
             print(f"Median Execution Time: {median_execution_time:.4f} seconds\n")
--- heuristics.py
+++ heuristics.py
@@ -3,18 +3,11 @@ from collections import defaultdict
 
 def random_choice(cnf):
     unassigned_literals = [literal for clause in cnf for literal in clause]
-    
     if not unassigned_literals:
         return None
-    
     return random.choice(unassigned_literals)
 
-'''def random_choice(cnf):
-    unassigned_literals = [literal for clause in cnf for literal in clause if -literal not in clause]
-    return random.choice(unassigned_literals)'''
-
 def two_clause(cnf):
-    # Count the occurrences of each literal in remaining clauses
     literal_count = {}
     for clause in cnf:
         for literal in clause:
@@ -22,25 +15,10 @@ def two_clause(cnf):
                 literal_count[literal] += 1
             else:
                 literal_count[literal] = 1
-
-    # Filter literals that appear in exactly two clauses
     two_clause_literals = [literal for literal, count in literal_count.items() if count == 2]
-
     if not two_clause_literals:
-        # No literals appear in exactly two clauses, return a random choice
         return random.choice(cnf[0]) if cnf else None
-
-    # Select a literal from those that appear in exactly two clauses
     return random.choice(two_clause_literals)
-
-'''def two_clause(cnf):
-    literal_count = defaultdict(int)
-    for clause in cnf:
-        if len(clause) == 2:
-            for literal in clause:
-                literal_count[abs(literal)] += 1
-    selected_literal = max(literal_count, key=literal_count.get)
-    return selected_literal'''
 
 def find_max_literal_weight(cnf):
     literal_weight = defaultdict(int)
@@ -48,11 +26,3 @@ def find_max_literal_weight(cnf):
         for literal in clause:
             literal_weight[abs(literal)] += 2 ** -len(clause)
     return max(literal_weight, key=literal_weight.get)
-
-def most_occurrences_heuristic(cnf):
-    literal_count = defaultdict(int)
-    for clause in cnf:
-        for literal in clause:
-            literal_count[abs(literal)] += 1
-    selected_literal = max(literal_count, key=literal_count.get)
-    return selected_literal
